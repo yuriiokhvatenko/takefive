087e24f9ca7df515843f8ca921a3189bf84db699
yuriiokhvatenko <113927739+yuriiokhvatenko@users.noreply.github.com>
2022-12-20 01:10:00 +0200

    add snow js animation

---
+++ src/js/snow3d.js
@@ -0,0 +1,12 @@
+https://cdn.jsdelivr.net/npm/christmas-snow-3d/build/snow3d.js
+
+// <script async src="https://cdn.jsdelivr.net/npm/christmas-snow-3d/build/snow3d.js"></script>
+
+
+document.head.appendChild((function() {
+    var s = document.createElement('script');
+    s.type = 'text/javascript';
+    s.async = true;
+    s.src = 'https://cdn.jsdelivr.net/npm/christmas-snow-3d/build/snow3d.js';
+    return s;
+})());
